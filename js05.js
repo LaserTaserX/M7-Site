58ad80f22646d37e858163083ecbd1231d1640a4
Alexander Frieders <afrieders@my.waketech.edu>
2026-02-23 13:37:25 -0500

    Finished

--- js05.js
+++ js05.js
@@ -4,11 +4,94 @@
       Chapter Case
 
       Application to generate a slide show
-      Author: 
-      Date:   
+      Author: Xander Frieders
+      Date:   2/23/26
 
       Filename: js05.js
 */
+
+window.addEventListener("load", createLightbox);
+
+function createLightbox() {
+   let lightbox = document.getElementById("lightbox");
+   let lbTitle = document.createElement("h1");
+   let lbCounter = document.createElement("div");
+   let lbPrev = document.createElement("div");
+   let lbNext = document.createElement("div");
+   let lbPlay = document.createElement("div");
+   let lbImages = document.createElement("div");
+   lightbox.appendChild(lbTitle);
+   lbTitle.id = "lbTitle";
+   lbTitle.textContent = lightboxTitle;
+   lightbox.appendChild(lbCounter);
+   lbCounter.id = "lbCounter";
+   let currentImg = 1;
+   lbCounter.textContent = currentImg + " / " + imgCount;
+   lightbox.appendChild(lbPrev);
+   lbPrev.id = "lbPrev";
+   lbPrev.innerHTML = "&#9664;";
+   lbPrev.onclick = showPrev;
+   lightbox.appendChild(lbNext);
+   lbNext.id = "lbNext";
+   lbNext.innerHTML = "&#9654;";
+   lbNext.onclick = showNext;
+   lightbox.appendChild(lbPlay);
+   lbPlay.id = "lbPlay";
+   lbPlay.innerHTML = "&#9199;";
+   let timeID;
+   lbPlay.onclick = function() {
+      if (timeID) {
+         window.clearInterval(timeID);
+         timeID = undefined;
+      } else {
+         showNext();
+         timeID = window.setInterval(showNext, 1500);
+      }
+   };
+   lightbox.appendChild(lbImages);
+   lbImages.id = "lbImages";
+
+   for (let i = 0; i < imgCount; i++) {
+      let image = document.createElement("img");
+      image.src = imgFiles[i];
+      image.alt = imgCaptions[i];
+      image.onclick = createOverlay;
+      lbImages.appendChild(image);
+   }
+
+   function showNext() {
+      lbImages.appendChild(lbImages.firstElementChild);
+      (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+      lbCounter.textContent = currentImg + " / " + imgCount;
+   }
+
+   function showPrev() {
+      lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+      (currentImg > 1) ? currentImg-- : currentImg = imgCount;
+      lbCounter.textContent = currentImg + " / " + imgCount;
+   }
+
+   function createOverlay() {
+      let overlay = document.createElement("div");
+      overlay.id = "lbOverlay";
+      let figureBox = document.createElement("figure");
+      overlay.appendChild(figureBox);
+      let overlayImage = this.cloneNode("true");
+      figureBox.appendChild(overlayImage);
+      let overlayCaption = document.createElement("figcaption");
+      overlayCaption.textContent = this.alt;
+      figureBox.appendChild(overlayCaption);
+      let closeBox = document.createElement("div");
+      closeBox.id = "lbOverlayClose";
+      closeBox.innerHTML = "&times;";
+      closeBox.onclick = function() {
+         document.body.removeChild(overlay);
+      };
+
+      figureBox.appendChild(closeBox);
+      document.body.appendChild(overlay);
+   }
+}
 
 window.addEventListener("load", setupGallery);
 
